d7cb2c6b24b6b57d5f3ee158a91bd7585c50f7e4
Gabriele Crea <cregg27@gmail.com>
2022-09-19 15:19:35 +0200

    Es8 Completo e funzionante

--- Es8.js
+++ Es8.js
@@ -2,3 +2,19 @@ const prompt= require ('prompt-sync')();
 
 let NumeroUtente= prompt("Inserire numero limite massimo: ");
 let k= parseInt(NumeroUtente);
+let divisione;
+
+
+for (let i=1; i<=k; i++) {
+    let counter=0;
+    for (let j=1; j<=i; j++) {
+        if (i%j==0) {
+        divisione= i/j;  
+        counter++;
+        //console.log(divisione + " ");
+        }
+    }
+    if (counter ==2) {
+        console.log("Il numero è primo!"+ i );
+    }
+}
