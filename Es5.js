a23edd380c53bd84d3636a9ffef5dfb7044c2d2c
Gabriele Crea <cregg27@gmail.com>
2022-09-20 14:53:38 +0200

    Es5 + BONUS

--- Es5.js
+++ Es5.js
@@ -1,18 +1,32 @@
 const prompt= require ('prompt-sync')();
 
-const Num1= 1;
 let Sommaintermedia= 0;
-let SommaTot= 0;
 
 let M= prompt("Inserire il numero finale desiderato: ");
+let CasoSwitch= prompt("Digita 1 per avere 3 e 7 come sottomultipli, altrimenti digita 2 ");
+let SottoMultiplo= prompt ("Scegli il secondo sottomultiplo: ");
 
-for (let i=1; i<=M; i++) {
-    if (i%3 == 0){
-    Sommaintermedia= Sommaintermedia + i;
-    console.log(Sommaintermedia);
-    } 
+switch (CasoSwitch)  {
+case 1 : 
+    for (let i=1; i<=M; i++) {
+        if (i%3 == 0 && i%7 == 0){
+        console.log(i);   
+        Sommaintermedia= Sommaintermedia + i;
+        //console.log(Sommaintermedia);
+        } 
+    }
+    console.log("La somma Tot è: " + Sommaintermedia);
+break;
+
+default: 
+    for (let i=1; i<=M; i++) {
+        if (i%3 == 0 && i%SottoMultiplo == 0){
+        console.log(i);   
+        Sommaintermedia= Sommaintermedia + i;
+        //console.log(Sommaintermedia);
+        } 
+    }
+    console.log("La somma Tot è: " + Sommaintermedia);
+break;
+
 }
-
-SommaTot= Sommaintermedia;
-console.log("La somma Tot è: ");
-console.log(SommaTot);
