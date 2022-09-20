753e9fa67e72f45345f4d7f62fff591ba837d84b
Gabriele Crea <cregg27@gmail.com>
2022-09-20 16:36:52 +0200

    Problema Logico con comparazione stringhe

--- Es10.js
+++ Es10.js
@@ -1,1 +1,17 @@
 const prompt= require ('prompt-sync')();
+
+let SceltaG1= prompt("Si scelga fra carta, forbice o sasso: ");
+console.log("Questa è la scelta del G1: " + SceltaG1);
+
+while (SceltaG1 != 'carta' || 'forbice' || 'sasso') {
+    console.log("Scelta non corretta");
+    console.log("Si prega di scegliere esattamente fra carta, forbice o sasso");
+}
+
+let SceltaG2= prompt("Si scelga fra carta, forbice o sasso");
+console.log("Questa è la scelta del G2" + SceltaG2);
+
+while (SceltaG2!= 'carta'|| 'forbice' || 'sasso') {
+    console.log("Scelta non corretta");
+    console.log("Si prega di scegliere esattamente fra carta, forbice o sasso");
+}
