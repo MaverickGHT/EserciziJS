d40858dd279f52fbdf8535eb26f8c1edd6f5f39d
Gabriele Crea <cregg27@gmail.com>
2022-09-20 15:56:46 +0200

    Es9

--- Es9.js
+++ Es9.js
@@ -1,1 +1,17 @@
 const prompt= require ('prompt-sync')();
+
+let Parole= ['Gatto', 'Cane', 'Pesce', 'Topo', 'Elefante', 'Mucca', 'Cavallo', 'Asino', 'Coniglio', 'Piccione', 'Lupo'];
+let i= Math.round(Math.random()*10);
+
+console.log(i);
+let Animale= Parole[i];
+
+let Scelta = prompt("indovina l'animale: Gatto, Cane, Pesce, Topo, Elefante, Mucca, Cavallo, Asino, Coniglio, Piccione, Lupo: ");
+
+console.log("Hai scelto: " + Scelta);
+
+if (Scelta==Animale) {
+    console.log("Complimenti hai indovinato!");
+} else {
+    console.log("Ritenta sarai più fortunato");
+}
