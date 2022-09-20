a700069c40230afefaeb5ae1f35ecd17c0eb6124
Gabriele Crea <cregg27@gmail.com>
2022-09-20 15:39:01 +0200

    ES7 + BONUS

--- Es7.js
+++ Es7.js
@@ -4,12 +4,16 @@ let PrimoFattore= 2;
 
 let UltimoFattore= prompt("Inserire il numero dell'ultima tabellina: ");
 let J= parseInt(UltimoFattore);
+let Escludi= parseInt (prompt("Inserire tabellina da escludere: "));
 
 for (let i=PrimoFattore; i<=J; i++) {
-    for (let k=1; k<=10; k++) {
-        let Tabellina= i*k;
-        console.log(Tabellina);
-    }
+    if (i!=Escludi){
+        for (let k=1; k<=10; k++) {
+            let Tabellina= i*k;
+            console.log(Tabellina);
+        }
     console.log("");
+    }
 }
+
 
