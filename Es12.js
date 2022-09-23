bc329efd0d738d37cd60138c25d5e671f065b66f
Gabriele Crea <cregg27@gmail.com>
2022-09-23 12:57:28 +0200

    Es12 parte 1

--- Es12.js
+++ Es12.js
@@ -1,2 +1,22 @@
 const prompt= require ('prompt-sync')();
 
+function Bisestile (x) {
+    if (x%4==0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let anno = parseInt(prompt("Inserire un anno: "));
+console.log ("L'anno inserito è: " + anno);
+
+let Bis = Bisestile(anno);
+console.log("Bisestile è: " + Bis);
+
+if (Bis == true) {
+    console.log("L'anno è bisestile");
+} else {
+    console.log("L'anno non è bisestile");
+}
+
