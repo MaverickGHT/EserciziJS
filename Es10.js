9b8c397d95888f9cf74ffa409153b8507dc13338
Gabriele Crea <cregg27@gmail.com>
2022-09-23 11:19:47 +0200

    While problematico

--- Es10.js
+++ Es10.js
@@ -1,17 +1,19 @@
 const prompt= require ('prompt-sync')();
 
-let SceltaG1= prompt("Si scelga fra carta, forbice o sasso: ");
+let SceltaG1= parseInt(prompt("Si scelga fra carta=1, forbice=2 o sasso=3: "));
 console.log("Questa è la scelta del G1: " + SceltaG1);
 
-while (SceltaG1 != 'carta' || 'forbice' || 'sasso') {
+while (SceltaG1 == 1) {
     console.log("Scelta non corretta");
     console.log("Si prega di scegliere esattamente fra carta, forbice o sasso");
+    let SceltaG1= parseInt(prompt("Si scelga fra carta=1, forbice=2 o sasso=3: "));
+    console.log("Questa è la scelta del G1: " + SceltaG1);
 }
 
-let SceltaG2= prompt("Si scelga fra carta, forbice o sasso");
+/*let SceltaG2= prompt("Si scelga fra carta, forbice o sasso");
 console.log("Questa è la scelta del G2" + SceltaG2);
 
 while (SceltaG2!= 'carta'|| 'forbice' || 'sasso') {
     console.log("Scelta non corretta");
-    console.log("Si prega di scegliere esattamente fra carta, forbice o sasso");
-}
+    let SceltaG1= parseInt(prompt("Si scelga fra carta=1, forbice=2 o sasso=3: "));
+} */
