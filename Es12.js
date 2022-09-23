a39163217c0152f33b5d869a9c1da26b433bb287
Gabriele Crea <cregg27@gmail.com>
2022-09-23 13:59:50 +0200

    Es12 + Bonus 1

--- Es12.js
+++ Es12.js
@@ -8,11 +8,36 @@ function Bisestile (x) {
     }
 }
 
+function Indovina (x, bis) {
+    if (bis == true && x == 1) {
+        console.log("Hai indovinato è bisestile");
+    } else if (bis == false && x == 1) {
+        console.log("Spiaze ma non è bisestile");
+    } else if (bis == true && x == 2) {
+        console.log("Spiaze ma è bisestile");
+    } else if(bis == false && x == 2) {
+        console.log("Corretto, non è bisestile");
+    }
+
+}
+
 let anno = parseInt(prompt("Inserire un anno: "));
-console.log ("L'anno inserito è: " + anno);
+//console.log ("L'anno inserito è: " + anno);
 
 let Bis = Bisestile(anno);
-console.log("Bisestile è: " + Bis);
+//console.log("Bisestile è: " + Bis);
+
+let sceltautente= parseInt(prompt("Prova a indovinare se è bisestile: 1 sì, 2 no: "));
+console.log("Scelta utente prima del while: " + sceltautente);
+
+while (sceltautente != 1) {
+    if (sceltautente!=2){
+    sceltautente= parseInt(prompt("Inserire solo 1 per sì o 2 per no"));
+    console.log("Scelta utente dopo errore: " + sceltautente);
+    } else {break;}
+}
+
+Indovina (sceltautente, Bis);
 
 if (Bis == true) {
     console.log("L'anno è bisestile");
