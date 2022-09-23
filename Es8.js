4f1f4fe1c240c821fb2a24d46a53681b136bb8ae
Gabriele Crea <cregg27@gmail.com>
2022-09-23 09:29:42 +0200

    Implementato un if più efficiente

--- Es8.js
+++ Es8.js
@@ -8,17 +8,20 @@ let counter2=0;
 for (let i=1; i<=k; i++) {
     let counter=0;
     for (let j=1; j<=i; j++) {
+        if (counter > 2) {break;}
+        else {
         if (i%j==0) {
         //divisione= i/j;  
         counter++;
         //console.log(divisione + " ");
         }
-    }
+     }    
+ }
     if (counter ==2) {
         console.log("Il numero è primo!"+ i );
         counter2++;
     }
-    
+
 }
 
 console.log(counter2);
