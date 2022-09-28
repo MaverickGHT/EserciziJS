c7360a085d82062c3759c363bc9f6cdc230c3af8
Gabriele <cregg27@gmail.com>
2022-09-28 09:17:16 +0200

    es 10 completo

--- Es10.js
+++ Es10.js
@@ -1,19 +1,41 @@
 const prompt= require ('prompt-sync')();
 
-let SceltaG1= parseInt(prompt("Si scelga fra carta=1, forbice=2 o sasso=3: "));
+let SceltaG1=0;
+
+ SceltaG1= prompt("G1 scelga fra carta, forbice o sasso: ").toLowerCase();
 console.log("Questa è la scelta del G1: " + SceltaG1);
 
-while (SceltaG1 == 1) {
+while ((SceltaG1 != "carta") && (SceltaG1 != "forbice") && (SceltaG1 != "sasso")) {
     console.log("Scelta non corretta");
-    console.log("Si prega di scegliere esattamente fra carta, forbice o sasso");
-    let SceltaG1= parseInt(prompt("Si scelga fra carta=1, forbice=2 o sasso=3: "));
+    SceltaG1= prompt("Si scelga fra carta, forbice o sasso: ");
     console.log("Questa è la scelta del G1: " + SceltaG1);
 }
 
-/*let SceltaG2= prompt("Si scelga fra carta, forbice o sasso");
-console.log("Questa è la scelta del G2" + SceltaG2);
+let SceltaG2= prompt("G2 scelga fra carta, forbice o sasso: ").toLowerCase();
+console.log("Questa è la scelta del G2: " + SceltaG2);
 
-while (SceltaG2!= 'carta'|| 'forbice' || 'sasso') {
+while ((SceltaG2 != "carta") && (SceltaG2 != "forbice") && (SceltaG2 != "sasso")) {
     console.log("Scelta non corretta");
-    let SceltaG1= parseInt(prompt("Si scelga fra carta=1, forbice=2 o sasso=3: "));
-} */
+    SceltaG2=prompt("Si scelga fra carta, forbice o sasso: ");
+    console.log("Questa è la scelta del G2: " + SceltaG2);
+} 
+
+if ((SceltaG1=="carta") && (SceltaG2== "carta")) {
+    console.log("PAREGGIO!");
+} else if ((SceltaG1=="carta") && (SceltaG2== "forbice")) {
+    console.log("Ha vinto G2!");
+} else if ((SceltaG1=="carta") && (SceltaG2== "sasso")) {
+    console.log("Ha vinto G1!");
+} else if ((SceltaG1=="forbice") && (SceltaG2== "forbice")) {
+    console.log("PAREGGIO!");
+} else if ((SceltaG1=="forbice") && (SceltaG2== "sasso")) {
+    console.log("Ha vinto G2!");
+} else if ((SceltaG1=="forbice") && (SceltaG2== "carta")) {
+    console.log("Ha vinto G1!");
+} else if ((SceltaG1=="sasso") && (SceltaG2== "sasso")) {
+    console.log("PAREGGIO!");
+} else if ((SceltaG1=="sasso") && (SceltaG2== "carta")) {
+    console.log("Ha vinto G2!");
+} else if ((SceltaG1=="sasso") && (SceltaG2== "forbice")) {
+    console.log("Ha vinto G1!");
+}
